80f658d4b66a95f35383078674c347e32b099e50
Aarya C <abchhatkuli@gmail.com>
2025-08-31 01:05:20 -0500

    website

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -9,9 +9,6 @@ const Footer = ({}) => {
           <Socials />
         </div>
       </div>
-      <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0 bg-white dark:bg-black">
-        Made With ❤️ by Aarya
-      </h1>
     </>
   );
 };
