633adcca63c89920a40bd9e77b761efa60f402d7
Aarya C <abchhatkuli@gmail.com>
2025-08-31 01:08:23 -0500

    twice

--- pages/index.js
+++ pages/index.js
@@ -91,7 +91,7 @@ export default function Home() {
               ref={textThree}
               className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5 bg-white dark:bg-black"
             >
-              {data.headerTaglineTwo}
+              {data.headerTaglineThree}
             </h1>
             <h1
               ref={textFour}
